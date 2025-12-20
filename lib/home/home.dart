f6d20872e44373a380e346d834e857bd002bbcbc
Cabdinasir64 <cabdinasira64@gmail.com>
2025-12-20 09:45:12 +0300

    feat: add Home widget with AppBar for the home page

---
+++ lib/home/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        backgroundColor: Colors.blueAccent[700],
+        titleTextStyle: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Arial',
+        ),
+      ),
+    );
+  }
+}
