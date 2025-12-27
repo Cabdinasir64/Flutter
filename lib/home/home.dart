906fe55382eac15de03cc29203637df511aa8bdb
Cabdinasir64 <cabdinasira64@gmail.com>
2025-12-27 09:58:44 +0300

    Update UI

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -30,9 +30,11 @@ class Home extends StatelessWidget {
             "https://images.unsplash.com/photo-1766445318570-1dc4bf3f5d79?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           ),
           Container(
-            margin: EdgeInsets.all(40),
             padding: EdgeInsets.all(10),
-            color: Colors.blue,
+            decoration: BoxDecoration(
+              color: Colors.brown,
+              borderRadius: BorderRadius.circular(10),
+            ),
             child: Text(
               "Beautiful Landscape",
               style: TextStyle(
