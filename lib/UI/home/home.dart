5644971f57f1776993d35f0aad718f4eac98ae5e
Cabdinasir64 <cabdinasira64@gmail.com>
2025-12-27 10:47:03 +0300

    Add Home widget implementation in home.dart

---
+++ lib/UI/home/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_app1/UI/home/body/body1.dart';
+import 'package:mobile_app1/UI/home/appBar/appBar1.dart';
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar1(),
+      body: Body1()
+    );
+  }
+}
