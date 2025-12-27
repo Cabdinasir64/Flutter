3ce739cef3bdd6cfdf25e0b991c1de42263e8292
Cabdinasir64 <cabdinasira64@gmail.com>
2025-12-27 10:47:21 +0300

    Add Body1 widget implementation in body1.dart

---
+++ lib/UI/home/body/body1.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class Body1 extends StatelessWidget {
+  const Body1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.network(
+          "https://images.unsplash.com/photo-1766445318570-1dc4bf3f5d79?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        ),
+        SizedBox(height: 20),
+        Container(
+          margin: EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.amberAccent,
+                ),
+                child: Text(
+                  "Beautiful Landscape",
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Nature at its best showcasing stunning views and colors in the environment that captivate the soul and inspire awe.",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+                "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w500,
+                  height: 1.5,
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.brown,
+                ),
+                child: Text(
+                  "More Details here ... here we can add more information about the landscape and its features.",
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
