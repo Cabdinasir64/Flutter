285c1a2d7c05bca40a3331e52682a59208082c24
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-03 10:32:50 +0300

    Update Ui

--- lib/UI/home/DrawerNaviagtion1.dart/home1.dart
+++ lib/UI/home/DrawerNaviagtion1.dart/home1.dart
@@ -14,7 +14,30 @@ class Home1 extends StatelessWidget {
             UserAccountsDrawerHeader(
               accountName: const Text('John Doe'),
               accountEmail: const Text('john.doe@example.com'),
-              currentAccountPicture: CircleAvatar(foregroundImage: AssetImage('assets/profile.jpg'),),
+              currentAccountPicture: CircleAvatar(
+                foregroundImage: AssetImage('assets/profile.jpg'),
+              ),
+              decoration: BoxDecoration(color: Colors.blue),
+            ),
+            ListTile(
+              leading: const Icon(Icons.group_rounded),
+              title: const Text('New Group'),
+            ),
+            ListTile(
+              leading: const Icon(Icons.person_rounded),
+              title: const Text('Contact'),
+            ),
+            ListTile(
+              leading: const Icon(Icons.call_rounded),
+              title: const Text('Call'),
+            ),
+            ListTile(
+              leading: const Icon(Icons.map_rounded),
+              title: const Text('People nearby'),
+            ),
+            ListTile(
+              leading: const Icon(Icons.bookmark_rounded),
+              title: const Text('Saved Messages'),
             ),
           ],
         ),
