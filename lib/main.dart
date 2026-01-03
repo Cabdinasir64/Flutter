801736ee29ea2586e3cad488fec43a6218a5df8f
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-03 09:59:30 +0300

    remove home

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/home1.dart';
-import 'package:mobile_app1/UI/home/BottomNavigationBar/home1.dart';
+// import 'package:mobile_app1/UI/home/BottomNavigationBar/home1.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Home1(),
   ));
 }
