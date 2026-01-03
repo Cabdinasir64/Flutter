223bc0fea1b9215d00143fdc3158335007c5b2de
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-03 10:04:14 +0300

    Insert new widget Stalless

---
+++ lib/UI/home/DrawerNaviagtion1.dart/home1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class Home1 extends StatelessWidget {
+  const Home1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Drawer Navigation Example1'),
+      ),
+    );
+  }
+}
