ae8238b1409cecd8253f9f274884ef99371581fe
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-03 10:24:14 +0300

    Update Ui

--- lib/UI/home/DrawerNaviagtion1.dart/home1.dart
+++ lib/UI/home/DrawerNaviagtion1.dart/home1.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 
-
 class Home1 extends StatelessWidget {
   const Home1({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Drawer Navigation Example1'),
+      appBar: AppBar(title: const Text('Drawer Navigation Example1')),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: const Text('John Doe'),
+              accountEmail: const Text('john.doe@example.com'),
+              currentAccountPicture: CircleAvatar(foregroundImage: AssetImage('assets/profile.png'),),
+            ),
+          ],
+        ),
       ),
     );
   }
