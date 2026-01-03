a8e09348b4a39586f9ff8c7d72e93a349f4ad7d3
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-03 10:03:59 +0300

    add home1

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/home1.dart';
 // import 'package:mobile_app1/UI/home/BottomNavigationBar/home1.dart';
+import 'package:mobile_app1/UI/home/DrawerNaviagtion1.dart/home1.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Home1()));
 }
