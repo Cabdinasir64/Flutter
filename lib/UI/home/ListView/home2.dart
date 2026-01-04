3c6187eeb161691e0ab847b3d94bdfe28459f49d
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-04 10:35:09 +0300

    update content

--- lib/UI/home/ListView/home2.dart
+++ lib/UI/home/ListView/home2.dart
@@ -7,7 +7,7 @@ class Home2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("List View Example1"),
+        title: Text("List View Example2"),
         shadowColor: Colors.black,
         elevation: 4,
         titleTextStyle: TextStyle(
@@ -18,58 +18,25 @@ class Home2 extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            color: Colors.red,
-            height: 100,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            color: Colors.yellow,
-            height: 100,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            color: Colors.pink,
-            height: 100,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            color: Colors.orange,
-            height: 100,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            color: Colors.blue,
-            height: 100,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            color: Colors.redAccent,
-            height: 100,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            color: Colors.black,
-            height: 100,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            color: Colors.blueAccent,
-            height: 100,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            color: Colors.pinkAccent,
-            height: 100,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            color: Colors.yellowAccent,
-            height: 100,
-          ),
+          ContainerWidget(),
+          ContainerWidget(),
+          ContainerWidget(),
+          ContainerWidget(),
         ],
       ),
+    );
+  }
+}
+
+class ContainerWidget extends StatelessWidget {
+  const ContainerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      color: Colors.yellow,
+      height: 100,
     );
   }
 }
