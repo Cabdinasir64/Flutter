fa2a44c5083af3bad0af09859031e37812fbe29c
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-04 11:55:54 +0300

    update UI

--- lib/UI/home/ListView/home3.dart
+++ lib/UI/home/ListView/home3.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'document/students1.dart';
 
 class Home3 extends StatelessWidget {
   const Home3({super.key});
@@ -7,40 +8,41 @@ class Home3 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("List View Example3"),
-        shadowColor: Colors.black,
-        elevation: 4,
-        titleTextStyle: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          color: Colors.black,
+        title: const Text(
+          "List View Example 3",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
         ),
+        backgroundColor: Colors.blue,
+        elevation: 4,
       ),
-      body: ListView(
-        scrollDirection: Axis.horizontal,
-        children: [
-          ContainerWidget(),
-          ContainerWidget(),
-          ContainerWidget(),
-          ContainerWidget(),
-          ContainerWidget(),
-          ContainerWidget(),
-        ],
+      body: ListView.builder(
+        itemCount: students1.length,
+        itemBuilder: (context, index) {
+          return Card(
+            margin: EdgeInsets.all(8),
+            elevation: 3,
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: Text(
+                  students1[index]['name'][0],
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+              title: Text(
+                students1[index]['name'],
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text("Age: ${students1[index]['age']}"),
+              trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+            ),
+          );
+        },
       ),
-    );
-  }
-}
-
-class ContainerWidget extends StatelessWidget {
-  const ContainerWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(10),
-      color: Colors.yellow,
-      height: 10,
-      width: 10,
     );
   }
 }
