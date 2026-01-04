6cc6fbf5fc7668e63826871b9a27488a34351abf
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-04 09:45:58 +0300

    update location

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/home1.dart';
 // import 'package:mobile_app1/UI/home/BottomNavigationBar/home1.dart';
+// import 'package:mobile_app1/UI/home/DrawerNaviagtion1.dart/home1.dart';
 import 'package:mobile_app1/UI/home/ListView/home1.dart';
 
 void main() {
