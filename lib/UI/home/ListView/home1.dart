1ffed2286a88efef472ed3f248f5d1cf9b14a90b
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-04 09:51:17 +0300

    Update UI

--- lib/UI/home/ListView/home1.dart
+++ lib/UI/home/ListView/home1.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
-
 class Home1 extends StatelessWidget {
   const Home1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("List View Example1"),
+        titleTextStyle: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Colors.black,
+        ),
+      ),
+    );
   }
 }
