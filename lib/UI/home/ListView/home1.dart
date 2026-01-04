08c76f8108c47937079c34eeb4ed11311f6d512c
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-04 09:46:13 +0300

    insert StatelessWidget

---
+++ lib/UI/home/ListView/home1.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class Home1 extends StatelessWidget {
+  const Home1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
