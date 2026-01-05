3b07963e2c91279b2bb4602be7a4b0539dadc905
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-05 11:29:17 +0300

    added new home

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/BottomNavigationBar/home1.dart';
 // import 'package:mobile_app1/UI/home/DrawerNaviagtion1.dart/home1.dart';
 // import 'package:mobile_app1/UI/home/ListView/home1.dart';
-import 'package:mobile_app1/UI/home/ListView/home3.dart';
+// import 'package:mobile_app1/UI/home/ListView/home3.dart';
+import 'package:mobile_app1/UI/home/appBar/home1.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Home3()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Home1()));
 }
