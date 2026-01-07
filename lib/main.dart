f7e8a4567dd32ea55a1e37b919039247c542d494
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-07 08:53:03 +0300

    update main.dart to use Body5 widget instead of Body4

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/ListView/home3.dart';
 // import 'package:mobile_app1/UI/home/appBar/home1.dart';
 // import 'package:mobile_app1/UI/home/appBar/home2.dart';
-import 'package:mobile_app1/UI/home/body/body4.dart';
+import 'package:mobile_app1/UI/home/body/body5.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Body4()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Body5()));
 }
