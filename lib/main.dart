6b1818ead6e82025b2cad744d0e86bc8c36274e3
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-07 09:07:17 +0300

    update main.dart to use Body7 widget instead of Body6

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/ListView/home3.dart';
 // import 'package:mobile_app1/UI/home/appBar/home1.dart';
 // import 'package:mobile_app1/UI/home/appBar/home2.dart';
-import 'package:mobile_app1/UI/home/body/body6.dart';
+import 'package:mobile_app1/UI/home/body/body7.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Body6()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Body7()));
 }
