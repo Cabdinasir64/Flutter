2f32b8347a8a63703a57bb9d74ab4c3c24df01dd
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-07 08:50:47 +0300

    update main.dart to use Body4 widget instead of Body4

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/ListView/home3.dart';
 // import 'package:mobile_app1/UI/home/appBar/home1.dart';
 // import 'package:mobile_app1/UI/home/appBar/home2.dart';
-import 'package:mobile_app1/UI/home/body/body3.dart';
+import 'package:mobile_app1/UI/home/body/body4.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Body3()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Body4()));
 }
