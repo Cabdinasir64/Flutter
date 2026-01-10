4fc2eef6cb8f1db476420d667b7f70e7a90f177e
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-10 08:44:34 +0300

    update main.dart to use Body18 widget instead of Body17

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/ListView/home3.dart';
 // import 'package:mobile_app1/UI/home/appBar/home1.dart';
 // import 'package:mobile_app1/UI/home/appBar/home2.dart';
-import 'package:mobile_app1/UI/home/body/body17.dart';
+import 'package:mobile_app1/UI/home/body/body18.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Body17()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Body18()));
 }
