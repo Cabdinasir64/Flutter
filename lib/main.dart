3b8aef6b1dccdc05a33ba64be1377e7f78c431b2
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-18 10:08:55 +0300

    fix: correct import statement for Action7 screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/appBar/home2.dart';
 // import 'package:mobile_app1/UI/home/body/body21.dart';
 // import 'package:mobile_app1/UI/home/forms/form4.dart';
-import 'package:mobile_app1/UI/home/actions/action7dart';
+import 'package:mobile_app1/UI/home/actions/action7.dart';
 
 void main() {
   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Action7()));
