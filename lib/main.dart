487e6d8b6ff0a8bac17bd6117d15458d54a58cfe
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-18 10:07:50 +0300

    update: add action7 and set it as main home screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,8 @@ import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/appBar/home2.dart';
 // import 'package:mobile_app1/UI/home/body/body21.dart';
 // import 'package:mobile_app1/UI/home/forms/form4.dart';
-import 'package:mobile_app1/UI/home/actions/action6.dart';
+import 'package:mobile_app1/UI/home/actions/action7dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Action6()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Action7()));
 }
