baba652b8546d2037e587a6ecedd035bb32a4e2e
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-24 09:59:00 +0300

    update: add action20 and set it as main home screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,8 @@ import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/appBar/home2.dart';
 // import 'package:mobile_app1/UI/home/body/body21.dart';
 // import 'package:mobile_app1/UI/home/forms/form4.dart';
-import 'package:mobile_app1/UI/home/actions/action19.dart';
+import 'package:mobile_app1/UI/home/actions/action20.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Action19()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Action20()));
 }
