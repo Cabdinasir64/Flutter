fced1fc4cbca4c868fb12e9fca1a28e18a1e40ae
Cabdinasir64 <cabdinasira64@gmail.com>
2026-01-27 09:44:18 +0300

    refactor: update main.dart to use AssetsManagement1 as the main route

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,16 @@ import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/body/body21.dart';
 // import 'package:mobile_app1/UI/home/forms/form4.dart';
 // import 'package:mobile_app1/UI/home/actions/action21.dart';
-import 'package:mobile_app1/UI/home/navigations/navigation10.dart';
-import 'package:mobile_app1/UI/home/navigations/pages/user_details_page.dart';
+// import 'package:mobile_app1/UI/home/navigations/navigation10.dart';
+// import 'package:mobile_app1/UI/home/navigations/pages/user_details_page.dart';
+import 'package:mobile_app1/UI/home/assets/asset1.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        '/': (context) => const Navigation10(),
-        UserDetailsPage.routeName: (context) => const UserDetailsPage(),
+        '/': (context) => const AssetsManagement1(),
       },
     ),
   );
