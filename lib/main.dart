7b3c3c261dd29bba8205f210934be478f0aebaec
Cabdinasir64 <cabdinasira64@gmail.com>
2026-02-01 10:17:42 +0300

    feat: update main.dart to launch Responsive10 screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,14 @@ import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/navigations/navigation10.dart';
 // import 'package:mobile_app1/UI/home/navigations/pages/user_details_page.dart';
 // import 'package:mobile_app1/UI/home/assets/asset3.dart';
-import 'package:mobile_app1/UI/home/responsive/responsive9.dart';
+import 'package:mobile_app1/UI/home/responsive/responsive10.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        '/': (context) => const Responsive9(),
+        '/': (context) => const Responsive10(),
       },
     ),
   );
