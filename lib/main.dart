ffbb8e31abf3c1ccd3bd3a4c5c7428472aef3cc1
Cabdinasir64 <cabdinasira64@gmail.com>
2026-02-01 10:07:26 +0300

    feat: update main.dart to launch Responsive8 screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,14 @@ import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/navigations/navigation10.dart';
 // import 'package:mobile_app1/UI/home/navigations/pages/user_details_page.dart';
 // import 'package:mobile_app1/UI/home/assets/asset3.dart';
-import 'package:mobile_app1/UI/home/responsive/responsive7.dart';
+import 'package:mobile_app1/UI/home/responsive/responsive8.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        '/': (context) => const Responsive7(),
+        '/': (context) => const Responsive8(),
       },
     ),
   );
