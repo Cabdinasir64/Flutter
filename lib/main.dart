d889e30fdca31075cb07530e877a7cc9a0736b86
Cabdinasir64 <cabdinasira64@gmail.com>
2026-02-01 10:11:50 +0300

    feat: update main.dart to launch Responsive9 screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,14 @@ import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/navigations/navigation10.dart';
 // import 'package:mobile_app1/UI/home/navigations/pages/user_details_page.dart';
 // import 'package:mobile_app1/UI/home/assets/asset3.dart';
-import 'package:mobile_app1/UI/home/responsive/responsive8.dart';
+import 'package:mobile_app1/UI/home/responsive/responsive9.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        '/': (context) => const Responsive8(),
+        '/': (context) => const Responsive9(),
       },
     ),
   );
