50928923aed465a8721b0838d69c3987626f2be9
Cabdinasir64 <cabdinasira64@gmail.com>
2026-02-07 10:16:30 +0300

    update: set Api3 as the main entry screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,14 @@ import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/navigations/navigation10.dart';
 // import 'package:mobile_app1/UI/home/navigations/pages/user_details_page.dart';
 // import 'package:mobile_app1/UI/home/assets/asset3.dart';
-import 'package:mobile_app1/UI/home/api_integration/api2.dart';
+import 'package:mobile_app1/UI/home/api_integration/api3.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        '/': (context) => const Api2(),
+        '/': (context) => const Api3(),
       },
     ),
   );
