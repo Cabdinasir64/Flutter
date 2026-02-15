26daa6aba57763171d1b9a203bff23342fe7b396
Cabdinasir64 <cabdinasira64@gmail.com>
2026-02-15 08:47:24 +0300

    waa files cusub app colr ka custom ah

---
+++ lib/UI/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF3B82F6);
+  static const Color secondary = Color(0xFF64748B);
+  static const Color success = Color(0xFF22C55E); 
+  static const Color danger = Color(0xFFEF4444); 
+  static const Color warning = Color(0xFFF59E0B); 
+
+  static const Color bgLight = Color(0xFFF8FAFC); 
+  static const Color textMain = Color(0xFF1E293B); 
+  static const Color textMuted = Color(0xFF94A3B8); 
+}
