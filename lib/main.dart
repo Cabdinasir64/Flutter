56061a4fc3598671b4e8d69dc882443e01568da0
Cabdinasir64 <cabdinasira64@gmail.com>
2026-02-21 10:08:26 +0300

    waxaan ku soo darney screen ka theme8

--- lib/main.dart
+++ lib/main.dart
@@ -17,16 +17,16 @@ import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/api_integration/api5_task_detail.dart';
 // import 'package:mobile_app1/UI/home/api_integration/api5_search.dart';
 // import 'package:mobile_app1/UI/home/theme_demo/theme6.dart';
-import 'package:mobile_app1/UI/home/theme_demo/theme7.dart';
+import 'package:mobile_app1/UI/home/theme_demo/theme8.dart';
 
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/theme7',
+      initialRoute: '/theme8',
       routes: {
-        '/theme7': (context) => const Theme7(),
+        '/theme8': (context) => const Theme8(),
       },
     ),
   );
