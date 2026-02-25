9f684f5aeb53bab47096b22bb42e8ee9215fe38e
Cabdinasir64 <cabdinasira64@gmail.com>
2026-02-25 09:44:24 +0300

    waxaan ku soo darey screen cusub theme18

--- lib/main.dart
+++ lib/main.dart
@@ -17,16 +17,16 @@ import 'package:flutter/material.dart';
 // import 'package:mobile_app1/UI/home/api_integration/api5_task_detail.dart';
 // import 'package:mobile_app1/UI/home/api_integration/api5_search.dart';
 // import 'package:mobile_app1/UI/home/theme_demo/theme6.dart';
-import 'package:mobile_app1/UI/home/theme_demo/theme17.dart';
+import 'package:mobile_app1/UI/home/theme_demo/theme18.dart';
 
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/theme17',
+      initialRoute: '/theme18',
       routes: {
-        '/theme17': (context) => const Theme17(),
+        '/theme18': (context) => const Theme18(),
       },
     ),
   );
