ac0866f00c7074fd934e672341789ce3e01f12ea
Cabdinasir64 <cabdinasira64@gmail.com>
2026-03-02 10:21:30 +0300

    ku soo darey screen state4UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:mobile_app1/state_management/state3.dart';
-import 'package:mobile_app1/UI/home/state_examples/state3.dart';
+import 'package:mobile_app1/state_management/state4.dart';
+import 'package:mobile_app1/UI/home/state_examples/state4.dart';
 
 void main() {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => CartProvider3())
+        ChangeNotifierProvider(create: (_) => SettingsProvider4())
       ],
       child: const MyApp(),
     ),
@@ -21,8 +21,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/state3',
-      routes: {'/state3': (context) => State3UI()},
+      initialRoute: '/state4',
+      routes: {'/state4': (context) => State4UI()},
     );
   }
 }
