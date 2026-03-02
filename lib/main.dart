c575cfa6f236b61344231a848b2a7b846f52a12c
Cabdinasir64 <cabdinasira64@gmail.com>
2026-03-02 10:36:15 +0300

    ku soo darey screen state5 UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:mobile_app1/state_management/state4.dart';
-import 'package:mobile_app1/UI/home/state_examples/state4.dart';
+import 'package:mobile_app1/state_management/state5.dart';
+import 'package:mobile_app1/UI/home/state_examples/state5.dart';
 
 void main() {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => SettingsProvider4())
+        ChangeNotifierProvider(create: (_) => AuthProvider5())
       ],
       child: const MyApp(),
     ),
@@ -21,8 +21,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/state4',
-      routes: {'/state4': (context) => State4UI()},
+      initialRoute: '/state5',
+      routes: {'/state5': (context) => State5UI()},
     );
   }
 }
