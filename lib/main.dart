0d54dd5f855aab108a9222433fd37ab938c86a68
Cabdinasir64 <cabdinasira64@gmail.com>
2026-03-02 09:36:31 +0300

    ku soo darey screen cusub state1

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,26 @@
 import 'package:flutter/material.dart';
-// import 'package:mobile_app1/UI/home/home1.dart';
-// import 'package:mobile_app1/UI/home/BottomNavigationBar/home1.dart';
-// import 'package:mobile_app1/UI/home/DrawerNaviagtion1.dart/home1.dart';
-// import 'package:mobile_app1/UI/home/ListView/home1.dart';
-// import 'package:mobile_app1/UI/home/ListView/home3.dart';
-// import 'package:mobile_app1/UI/home/appBar/home1.dart';
-// import 'package:mobile_app1/UI/home/appBar/home2.dart';
-// import 'package:mobile_app1/UI/home/body/body21.dart';
-// import 'package:mobile_app1/UI/home/forms/form4.dart';
-// import 'package:mobile_app1/UI/home/actions/action21.dart';
-// import 'package:mobile_app1/UI/home/navigations/navigation10.dart';
-// import 'package:mobile_app1/UI/home/navigations/pages/user_details_page.dart';
-// import 'package:mobile_app1/UI/home/assets/asset3.dart';
-// import 'package:mobile_app1/UI/home/api_integration/api5.dart';
-// import 'package:mobile_app1/UI/home/api_integration/api5_form.dart';
-// import 'package:mobile_app1/UI/home/api_integration/api5_task_detail.dart';
-// import 'package:mobile_app1/UI/home/api_integration/api5_search.dart';
-// import 'package:mobile_app1/UI/home/theme_demo/theme6.dart';
-import 'package:mobile_app1/UI/home/theme_demo/theme24.dart';
-
+import 'package:provider/provider.dart';
+import 'package:mobile_app1/state_management/state1.dart';
+import 'package:mobile_app1/UI/home/state_examples/state1.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/theme24',
-      routes: {
-       '/theme24': (context) => const Theme24(),
-      },
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => CounterProvider1())],
+      child: const MyApp(),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/state1',
+      routes: {'/state1': (context) => const State1UI()},
+    );
+  }
 }
