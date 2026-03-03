c30d02e4bd6b79aada99f347692828edecf53221
Cabdinasir64 <cabdinasira64@gmail.com>
2026-03-03 10:22:21 +0300

    ku soo darey screen state7UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:mobile_app1/state_management/state6.dart';
-import 'package:mobile_app1/UI/home/state_examples/state6.dart';
+import 'package:mobile_app1/state_management/state7.dart';
+import 'package:mobile_app1/UI/home/state_examples/state7.dart';
 
 void main() {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => ProfileProvider6())
+        ChangeNotifierProvider(create: (_) => AuthProvider7()),
+        ProxyProvider<AuthProvider7, MessageProvider7>(
+          update: (context, auth, previousMessage) =>
+              MessageProvider7(auth.username),
+        ),
       ],
       child: const MyApp(),
     ),
@@ -21,8 +25,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/state6',
-      routes: {'/state6': (context) => State6UI()},
+      initialRoute: '/state7',
+      routes: {'/state7': (context) => State7UI()},
     );
   }
 }
