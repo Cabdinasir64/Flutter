ab69abb47e2fd14bfde2a718ddcd78bc1b664fbb
Cabdinasir64 <cabdinasira64@gmail.com>
2026-03-07 09:31:23 +0300

    ku soo daray state13 iyo ProviderScope halkii MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:mobile_app1/state_management/state12.dart';
-import 'package:mobile_app1/UI/home/state_examples/state12.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mobile_app1/UI/home/state_examples/state13.dart';
 
 void main() {
   runApp(
-    MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => CounterProvider12())],
-      child: const MyApp(),
+    const ProviderScope(
+      child: MyApp(),
     ),
   );
 }
@@ -19,8 +17,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/state12',
-      routes: {'/state12': (context) => const State12UI()},
+      initialRoute: '/state13',
+      routes: {
+        '/state13': (context) => const State13UI(),
+      },
     );
   }
 }
